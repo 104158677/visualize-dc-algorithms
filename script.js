33dc34c12325a3f7a8bdc3ae124d34395ce1be14
104158677 <s2527315@ed.ac.uk>
2024-06-19 11:19:19 +0100

    my commmit

---
+++ script.js
@@ -0,0 +1,20 @@
+var pic;
+var picid = 1;
+function press(sw) {
+    if (picid == -1 && sw != 2) {alert("This arrays is already sorted.\nPlease press reset button!");}
+    else{
+        if (sw == 0) {
+            picid = (picid == 7) ? 7 : picid +=1; 
+            pic = "pics/".concat(picid.toString(),".png");}
+         else  if (sw == 1) {
+            picid = (picid == 1) ? 1 : picid -=1; 
+            pic = "pics/".concat(picid.toString(),".png");}
+        else  if (sw == 2) {
+            picid = 1;
+            pic = "pics/".concat(picid.toString(),".png");}
+        else {
+            if (picid > 4) {picid = 4;}
+            pic = "pics/".concat(picid.toString(),"sorted",".png");
+            picid = -1;}}
+  document.getElementById('myImage').src = pic;
+};
