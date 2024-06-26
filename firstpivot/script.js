62765211371c03447a3a9bde85182b31e33e3dc9
104158677 <s2527315@ed.ac.uk>
2024-06-26 19:41:33 +0100

    finish free pivot choice

---
+++ firstpivot/script.js
@@ -0,0 +1,30 @@
+var pic;
+var picid = 1;
+function press(sw) {
+    if (picid == -1 && sw != 2) {alert("This arrays is already sorted.\nPlease press reset button!");}
+    else{
+        if (sw == 0) {
+            picid = (picid == 9) ? 9 : picid +=1; 
+            pic = "pics/".concat(picid.toString(),".png");}
+         else  if (sw == 1) {
+            picid = (picid == 1) ? 1 : picid -=1; 
+            pic = "pics/".concat(picid.toString(),".png");}
+        else  if (sw == 2) {
+            picid = 1;
+            pic = "pics/".concat(picid.toString(),".png");}
+        else {
+            if (picid > 5) {picid = 5;}
+            pic = "pics/".concat(picid.toString(),"sorted",".png");
+            picid = -1;}}
+  document.getElementById('myImage').src = pic;
+};
+
+function quiz() {
+    window.close();
+    window.open('../errorpage/error.html');
+}
+
+function back() {
+    window.close();
+    window.open('../main.html');
+}
--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-var pic;
-var picid = 1;
 function press(sw) {
-    if (picid == -1 && sw != 2) {alert("This arrays is already sorted.\nPlease press reset button!");}
-    else{
         if (sw == 0) {
-            picid = (picid == 7) ? 7 : picid +=1; 
-            pic = "pics/".concat(picid.toString(),".png");}
-         else  if (sw == 1) {
-            picid = (picid == 1) ? 1 : picid -=1; 
-            pic = "pics/".concat(picid.toString(),".png");}
+            window.open('firstpivot/main.html')}
+        else  if (sw == 1) {
+            window.open('lastpivot/main.html')}
         else  if (sw == 2) {
-            picid = 1;
-            pic = "pics/".concat(picid.toString(),".png");}
+            window.open('middlepivot/main.html')}
         else {
-            if (picid > 4) {picid = 4;}
-            pic = "pics/".concat(picid.toString(),"sorted",".png");
-            picid = -1;}}
-  document.getElementById('myImage').src = pic;
+            window.open('randompivot/main.html')}
+
 };
 
-function quiz() {
-    window.close();
-    window.open('errorpage/error.html');
-}
+
