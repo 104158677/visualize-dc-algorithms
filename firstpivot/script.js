ce8e672c46590c00168e480165f83908dedcba03
104158677 <s2527315@ed.ac.uk>
2024-07-10 00:39:40 +0100

    modification

--- firstpivot/script.js
+++ firstpivot/script.js
@@ -2,25 +2,27 @@ var pic;
 var picid = 1;
 function press(sw) {
         if (sw == 0) {
-            myarray = [9,10,12,15,19];
+            myarray = [10,11,13,16,20];
             if (picid <= 4) {picid += 1;}
             else if (myarray.includes(picid)) {alert("Problem solved! Try reset or back!")}
+            else if (picid == 5) {alert("No more division needed, try conquer or solve instead!")}
             else {alert("All sub-problems solved, try conquer instead!")}}
          else  if (sw == 1) {
             if (picid <= 4) {alert("Problem unsolved! Try divide instead!");}
             else if (myarray.includes(picid)) {alert("Problem solved! Try reset or back!")}
             else {picid += 1;}}
         else  if (sw == 2) {
-            if (picid <= 2) {picid += 9;}
-            else if (picid == 3) {picid += 10;}
-            else if (picid == 4) {picid += 12;}
+            if (picid <= 2) {picid += 10;}
+            else if (picid == 3) {picid += 11;}
+            else if (picid == 4) {picid += 13;}
+            else if (picid == 5) {picid += 1;}
             else if (myarray.includes(picid)) {alert("Problem solved! Try reset or back!")}
             else {alert("All sub-problems solved, try conquer instead!")}}
         else  if (sw == 3) {
-            if (picid == 10 || picid == 11) {picid -= 9;}
+            if (picid == 12 || picid == 11) {picid -= 10;}
             else if (picid == 1) {picid = 1;}
-            else if (picid == 13) {picid -= 10;}
-            else if (picid == 16) {picid -=12;}
+            else if (picid == 14) {picid -= 11;}
+            else if (picid == 17) {picid -=13;}
             else {picid -= 1;}}
         else {picid = 1;}
         pic = "pics/".concat(picid.toString(),".png");
